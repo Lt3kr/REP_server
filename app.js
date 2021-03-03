23a9e07821a512b20205ca99e08f822b07786c5d
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-03 08:59:52 +0100

    Updaterade med POST och några GET

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const staticdir = __dirname + "\\static\\";
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(staticdir));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(staticdir + "html\\index.html"));
+
+app.get("/contact", (req, res) =>
+    res.sendFile(staticdir + "html\\contact.html")
+);
+
+app.post("/contact", (res, req) =>{
+    console.log(req.body.Fname);
+    console.log(req.body.Date);
+    res.redirect("/");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
