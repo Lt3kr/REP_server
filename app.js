526eb05218261deb1d17ae7c0aefd0e70e02db6c
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2021-03-23 16:07:48 +0100

    Början på log in

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const databaseModule = require("./databaseModule")
+const databaseModule = require("./databaseModule");
 const app = express();
 const port = 3000;
 const staticdir = __dirname + "\\static\\";
-const användareModel = require("./användare")
+const användareModel = require("./användare");
+const bcrypt = require("bcryptjs");
 
 app.use(express.static(staticdir));
 app.use(express.urlencoded());
@@ -12,8 +13,22 @@ app.use(express.json());
 app.get('/', (req, res) => res.sendFile(staticdir + "html\\index.html"));
 
 app.post("/signUp", async (req, res) => {
-    användareModel.saveAnvändare(req.body.User, req.body.password)
-    res.redirect("/")
-})
+    const hasedPassword = await bcrypt.hash(req.body.password, 10);
+    användareModel.saveAnvändare(req.body.User,  hasedPassword);
+    res.redirect("/");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.post("/logIn", async (req, res) => {
+    const user = await användareModel.saveAnvändare(req.body.User);
+    await bcrypt.compare(req.body.password, user.password, (err, success) => {
+        if (err){
+            console.log(err);
+        }
+        
+        if (success) console.log("success");
+        else console.log("Fail");
+    });
+    res.redirect("/");
+});
